e2092fdf2fcabd8d3022d6be29bdd09fed422b8c
clarabernstein1010 <109279856+clarabernstein1010@users.noreply.github.com>
2023-03-24 17:41:35 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tamopstfstates"
-    storage_account_name = "tfstatedevops001"
+    storage_account_name = "tamopstf001"
     container_name       = "tfstatedevops"
-    key                  = "terraformgithubexample.tfstate"
+    key                  = "tfstatedevops.tfstate"
   }
 }
  
@@ -18,22 +18,22 @@ data "azurerm_client_config" "current" {}
  
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
-  name     = "tamops"
-  location = "eastus2"
+  name     = "rg-cb"
+  location = "uksouth"
 }
  
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = "eastus2"
-  resource_group_name = azurerm_resource_group.tamops.name
-}
+# #Create Virtual Network
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "tamops-vnet"
+#   address_space       = ["192.168.0.0/16"]
+#   location            = "eastus2"
+#   resource_group_name = azurerm_resource_group.tamops.name
+# }
  
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
-}
+# # Create Subnet
+# resource "azurerm_subnet" "subnet" {
+#   name                 = "subnet"
+#   resource_group_name  = azurerm_resource_group.tamops.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefix       = "192.168.0.0/24"
+# }
