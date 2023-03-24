d5ce2e778b17eda53bfe1473a6dfd88d9386733f
clarabernstein1010 <109279856+clarabernstein1010@users.noreply.github.com>
2023-03-24 17:32:44 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tamopstfstates"
-    storage_account_name = "tfstatedevops"
-    container_name       = "terraformgithubexample"
+    storage_account_name = "tfstatedevops001"
+    container_name       = "tfstatedevops"
     key                  = "terraformgithubexample.tfstate"
   }
 }
